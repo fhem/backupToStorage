ec4e32d2fbd999e7e8f43211e8643a602eb4a902
Marko Oldenburg <marko.oldenburg@cooltux.net>
2020-11-03 13:19:36 +0100

    change version

--- FHEM/98_backupToStorage.pm
+++ FHEM/98_backupToStorage.pm
@@ -179,7 +179,7 @@ sub backupToStorage_Initialize {
   ],
   "release_status": "devepolment",
   "license": "GPL_2",
-  "version": "v1.2.0",
+  "version": "v1.2.1",
   "author": [
     "Marko Oldenburg <fhemsupport@cooltux.net>"
   ],
